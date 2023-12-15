6324b7849d486c7ac7e9f3ad777bf686cc22abf3
Starry Knight <whiteh4tter@gmail.com>
2023-12-15 17:54:38 -0500

    adding dude09

---
+++ keyboards/handwired/starrykeebs/dude09/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Starry Knight (@astarryknight)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+//Vial
+#define VIAL_KEYBOARD_UID {0xEB, 0x64, 0xA4, 0x93, 0x3C, 0xDC, 0xF3, 0x9C}
+#define VIAL_UNLOCK_COMBO_ROWS {0,0}
+#define VIAL_UNLOCK_COMBO_ROWS {0,1}
